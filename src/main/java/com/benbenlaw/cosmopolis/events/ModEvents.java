9bcda991193c914ede3283c74c254908de14cdeb
benbenlaw <benlawrenceben@hotmail.co.uk>
2022-03-28 18:29:30 +0100

    changed spawning of structures in space

--- src/main/java/com/benbenlaw/cosmopolis/events/ModEvents.java
+++ src/main/java/com/benbenlaw/cosmopolis/events/ModEvents.java
@@ -1,7 +1,51 @@
 package com.benbenlaw.cosmopolis.events;
 
+import com.benbenlaw.cosmopolis.Cosmopolis;
+import com.benbenlaw.cosmopolis.item.ModItems;
+import net.minecraft.client.Minecraft;
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.common.Tags;
+import net.minecraftforge.event.world.BlockEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import java.util.UUID;
+
+@Mod.EventBusSubscriber(modid = Cosmopolis.MOD_ID)
+
 public class ModEvents {
 
+    /*
+
+    //All Ores Drop Essence
+    @SubscribeEvent
+    public static void allOreChance(BlockEvent.BreakEvent event) {
+        if (!event.getPlayer().level.isClientSide()) {
+            BlockPos blockPos = event.getPos(); //          Block block = event.getState().getBlock();
+            Level world = (Level) event.getWorld();
+            TextComponent msg = new TextComponent("/say hello");
+            UUID playerUUID = Minecraft.getInstance().player.getUUID();
+
+            if (event.getState().is(Tags.Blocks.ORES))  //CHANGE TO ORES
+
+                Minecraft.getInstance().player.sendMessage(msg, playerUUID);
+
+
+                    world.addFreshEntity(new ItemEntity(world, blockPos.getX(), blockPos.getY(), blockPos.getZ(),
+                            new ItemStack(ModItems.ASTEROID_ROCK.get())));     //event.getPlayer().addItem(new ItemStack(ModItems.ADVANCED_MOB_ESSENCE.get().asItem()));  //event.getPlayer().setSecondsOnFire(3);
+                    //new ItemStack(ModItems.BASIC_ORE_ESSENCE.get())));
+                }
+
+    }
+
+
+     */
 
 
 }
