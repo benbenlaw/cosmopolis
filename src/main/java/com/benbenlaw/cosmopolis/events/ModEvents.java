5272fe5b54fede296ddd43b6668efa1924a3ac68
benbenlaw <benlawrenceben@hotmail.co.uk>
2022-08-12 09:49:00 +0100

    6.0

--- src/main/java/com/benbenlaw/cosmopolis/events/ModEvents.java
+++ src/main/java/com/benbenlaw/cosmopolis/events/ModEvents.java
@@ -7,7 +7,12 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.Difficulty;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeInstance;
+import net.minecraft.world.entity.ai.attributes.AttributeMap;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.common.ForgeMod;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -20,34 +25,31 @@ import java.util.UUID;
 public class ModEvents {
 
 
-
     //Space Breathing
     @SubscribeEvent
     public static void spaceBreathing(TickEvent.PlayerTickEvent event) {
 
-        Level world = event.player.getLevel();
-        BlockPos blockPos = event.player.getOnPos();
+        if (!event.player.isCreative()) {
 
-        boolean head = event.player.getItemBySlot(EquipmentSlot.HEAD).is(ModTags.Items.SPACE_SUIT_HELMET);
-        boolean chestplate = event.player.getItemBySlot(EquipmentSlot.CHEST).is(ModTags.Items.SPACE_SUIT_CHESTPLATE);
-        boolean legs = event.player.getItemBySlot(EquipmentSlot.LEGS).is(ModTags.Items.SPACE_SUIT_LEGGINGS);
-        boolean boots = event.player.getItemBySlot(EquipmentSlot.FEET).is(ModTags.Items.SPACE_SUIT_BOOTS);
+            Level world = event.player.getLevel();
 
-        if (head && chestplate && boots && legs || world.getDifficulty().equals(Difficulty.PEACEFUL)) {}
+            boolean head = event.player.getItemBySlot(EquipmentSlot.HEAD).is(ModTags.Items.SPACE_SUIT_HELMET);
+            boolean chestplate = event.player.getItemBySlot(EquipmentSlot.CHEST).is(ModTags.Items.SPACE_SUIT_CHESTPLATE);
+            boolean legs = event.player.getItemBySlot(EquipmentSlot.LEGS).is(ModTags.Items.SPACE_SUIT_LEGGINGS);
+            boolean boots = event.player.getItemBySlot(EquipmentSlot.FEET).is(ModTags.Items.SPACE_SUIT_BOOTS);
 
-        else if (event.player.getLevel().dimension().equals(ModDimensions.SPACE)) {
-            event.player.hurt(DamageSource.OUT_OF_WORLD, 1);
+            if (head && chestplate && boots && legs || world.getDifficulty().equals(Difficulty.PEACEFUL)) {
+            }
 
-            /*
-            world.addFreshEntity(new ItemEntity(world, blockPos.getX(), blockPos.getY(), blockPos.getZ(),
-                    new ItemStack(ModItems.ASTEROID_ROCK.get())));
+            else if (event.player.getLevel().dimension().equals(ModDimensions.SPACE)) {
+                event.player.hurt(DamageSource.OUT_OF_WORLD, 1);
 
- event.player.M
-             */
-
+            }
         }
-
+    }
 }
+
+
 
 
 
@@ -78,4 +80,3 @@ public class ModEvents {
      */
 
 
-}
