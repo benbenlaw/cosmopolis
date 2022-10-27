0968ef4a677676cc6bcffceecad7e6455518c801
HW9636 <9636dev@gmail.com>
2022-10-27 20:37:58 +0200

    Added UFOEntity Events

---
+++ src/main/java/com/benbenlaw/cosmopolis/events/ModBusEvents.java
@@ -0,0 +1,32 @@
+package com.benbenlaw.cosmopolis.events;
+
+import com.benbenlaw.cosmopolis.Cosmopolis;
+import com.benbenlaw.cosmopolis.entity.ModEntities;
+import com.benbenlaw.cosmopolis.entity.UFOModel;
+import com.benbenlaw.cosmopolis.entity.UFORenderer;
+import com.benbenlaw.cosmopolis.entity.custom.UFOEntity;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = Cosmopolis.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class ModBusEvents {
+    @SubscribeEvent
+    public static void onAttributeCreate(EntityAttributeCreationEvent event) {
+        event.put(ModEntities.UFO.get(), UFOEntity.createAttributes().build());
+    }
+
+    @SubscribeEvent
+    public static void onRegisterLayers(EntityRenderersEvent.RegisterLayerDefinitions event) {
+        event.registerLayerDefinition(UFOModel.LAYER_LOCATION, UFOModel::createBodyLayer);
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @SubscribeEvent
+    public static void onRegisterRenderer(EntityRenderersEvent.RegisterRenderers event) {
+        event.registerEntityRenderer(ModEntities.UFO.get(), UFORenderer::new);
+    }
+}
